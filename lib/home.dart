1b589db90795676ed1a66e50bdf4e74e510fa915
nehegeb <git@nehegeb.de>
2018-11-28 22:30:29 +0100

    102step6 card collection
    Replaced the basic card with a function to automatically generate cards as needed. Also implemented products to be displayed on those cards for the listing.

--- lib/home.dart
+++ lib/home.dart
@@ -13,9 +13,81 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
-  // TODO: Make a collection of cards (102)
+  
+  // Function to generate a list of cards.
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString()
+    );
+
+    // The card itself.
+    return products.map((product) {
+      return Card(
+        // TODO: Adjust card heights (103)
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+
+            // THe image of the product.
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget> [
+                    // TODO: Handle overflowing labels (103)
+
+                    // The name of the product.
+                    Text(
+                      product.name,
+                      style: theme.textTheme.title,
+                      maxLines: 1,
+                    ),
+                    // Seperator.
+                    SizedBox(height: 8.0),
+                    // The price of the product.
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.body2,
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      );
+    }).toList();
+
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -62,44 +134,12 @@ class HomePage extends StatelessWidget {
 
       ),
 
+      // The body containing a listing of cards.
       body: GridView.count(
         crossAxisCount: 2,
         padding: EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        children: <Widget>[
-
-          // Cards in the listing.
-          Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-
-                // The image on the card.
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget> [
-
-                      // The text on the card.
-                      Text('Title'),
-                      SizedBox(height: 8.0),
-                      Text('Secondary Text'),
-
-                    ],
-                  ),
-                ),
-
-              ],
-            ),
-          )
-
-        ],
+        children: _buildGridCards(context),
       ),
     );
   }
