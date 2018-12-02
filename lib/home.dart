ed55db461b07bd14d8d3c832e185ccff5b8a35d4
nehegeb <git@nehegeb.de>
2018-12-02 15:31:08 +0100

    Revert "104step4 assymetric view"
    This reverts commit 9237223c082a8c174f97e17c51292ad391ee5a8e.

--- lib/home.dart
+++ lib/home.dart
@@ -23,6 +23,34 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AsymmetricView(products: ProductsRepository.loadProducts(category));
+  // TODO: Return an AsymmetricView (104)
+  // TODO: Pass Category variable to AsymmetricView (104)
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.light,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        title: Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.tune),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
+      ),
+      body: AsymmetricView(products: ProductsRepository.loadProducts(Category.all)),
+    );
   }
 }
